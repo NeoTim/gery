8edaa6081a42dd36e648a7b4c3f44686f08bf54d
forsigner <forsigner@gmail.com>
2018-12-06 10:33:41 +0800

    test: test query without client

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,29 +1,64 @@
-import fetchMock from 'fetch-mock'
+import { GraphQLClient, query, gql } from '../src'
 
-import { Client, gql } from '../src'
+interface Person {
+  Person: {
+    id: string
+    name: string
+  }
+}
+
+interface AllPersons {
+  allPersons: Array<Person['Person']>
+}
+
+const endpoint = 'https://api.graph.cool/simple/v1/swapi'
+
+const QUERY_PERSON = gql`
+  {
+    allPersons {
+      id
+      name
+    }
+  }
+`
+
+interface Person {
+  Person: {
+    id: string
+    name: string
+  }
+}
+const GET_PERSON = gql`
+  query getPerson($id: ID) {
+    Person(id: $id) {
+      id
+      name
+    }
+  }
+`
+
+test('can query by client', async () => {
+  const client = new GraphQLClient({ endpoint })
+  const data = await client.query<AllPersons>(QUERY_PERSON)
+  expect(typeof data.allPersons.length).toBe('number')
+})
 
 test('can query', async () => {
-  const endpoint = 'http://example.com/graphql'
+  const data = await query<AllPersons>(endpoint, QUERY_PERSON)
+  expect(typeof data.allPersons.length).toBe('number')
+})
 
-  const response = {
-    data: {
-      foo: 'bar',
-    },
+test('use varialbles', async () => {
+  const data = await query<Person>(endpoint, GET_PERSON, {
+    id: 'cj0nv9peiewhf013011i9a4h2',
+  })
+  expect(data.Person.id).toBe('cj0nv9peiewhf013011i9a4h2')
+})
+
+test('error', async () => {
+  try {
+    await query<Person>(endpoint, 'ERROR')
+  } catch (error) {
+    expect(!!error).toBe(true)
   }
-  const gqlStr = gql`
-    {
-      foo
-    }
-  `
-
-  fetchMock.mock(endpoint, response)
-
-  const data1: typeof response = await fetch(endpoint).then(r => r.json())
-
-  const client = new Client(endpoint)
-  const data2 = await client.query<{ foo: string }>(gqlStr)
-
-  expect(data1.data.foo).toBe(data2.foo)
-
-  fetchMock.restore()
 })
